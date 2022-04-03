6bad974fad8ac928eb0e87930e1981a628206473
Ruslan Sayfullin <ruslansaifullin91@gmail.com>
2022-04-03 22:54:16 +0500

    drf-yasg донастроили

--- catalog/catalog/settings.py
+++ catalog/catalog/settings.py
@@ -24,7 +24,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'debug_toolbar',
-    'drf-yasg',
+    'drf_yasg',
 ] + ['handbook.apps.HandbookConfig',
     ]
 
--- catalog/catalog/yasg.py
+++ catalog/catalog/yasg.py
@@ -1,24 +1,24 @@
-from django.urls import path
-import drf_yasg
+from django.urls import re_path
 from rest_framework import permissions
 from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
 
-"""настройка drf-yasg автодокументирование api django rest framework"""
-schema_view = get_schema_view(
-    openapi.Info(
-        title='Python libs catalog',
-        default_version='v1',
-        description="Опсание сервиса терминологии и REST API",
-        license=openapi.License(name="BSD License"),
-    ),
-    public=True,
-    permission_classes=(permissions.AllowAny,)
 
+schema_view = get_schema_view(
+   openapi.Info(
+      title="Libs API",
+      default_version='v1',
+      description="Test description",
+      terms_of_service="https://www.google.com/policies/terms/",
+      contact=openapi.Contact(email="contact@snippets.local"),
+      license=openapi.License(name="BSD License"),
+   ),
+   public=True,
+   permission_classes=[permissions.AllowAny],
 )
 
 urlpatterns = [
-    path('swagger(?P<format>\.json|\yaml)', schema_view.without_ui(cache_timeout=0), name='schema-json'),
-    path('swagger/',  schema_view.without_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
-    path('redoc/',  schema_view.without_ui('redoc', cache_timeout=0), name='schema-redoc'),
+   re_path(r'^swagger(?P<format>\.json|\.yaml)', schema_view.without_ui(cache_timeout=0), name='schema-json'),
+   re_path(r'^swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+   re_path(r'^redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
 ]
