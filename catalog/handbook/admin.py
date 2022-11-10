9aa47fe9eab712198efd501682ac0cfda5839008
Ruslan Sayfullin <ruslansaifullin91@gmail.com>
2022-11-10 13:24:48 +0500

    perl fast exponention

--- catalog/handbook/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-
-from .models import Directory, DirectoryVersion, DirectoryElement
-
-admin.site.register(Directory)
-
-admin.site.register(DirectoryVersion)
-
-admin.site.register(DirectoryElement)
