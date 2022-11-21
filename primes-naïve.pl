2922d9ce17724759ebbcb97c3a9b925a2ce108b6
Ruslan Sayfullin <ruslansaifullin91@gmail.com>
2022-11-21 09:28:43 +0500

    Поиск простых чисел, Наивный (сплошной) перебор делителей.

---
+++ primes-naïve.pl
@@ -0,0 +1,22 @@
+#!/usr/bin/perl
+
+use warnings;
+
+my $n=shift or die "$0: Укажите число в командной строке\n";
+
+print "2\n" if $n>=2;
+
+for(my $i=3; $i<=$n; $i+=2)
+{
+	my $flag=1;
+	for(my $j=3; $j<$i; $j+=2)
+	{
+		if($i % $j==0)
+		{
+			$flag=0;
+			last;
+		}
+	}
+	print "$i\n" if $flag;
+}
+
